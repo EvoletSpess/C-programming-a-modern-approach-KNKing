21f1d1b9934e2aecae86bd69565e61bb67660a4e
Aleksandr F <uni.fedotov@gmail.com>
2024-03-21 23:38:39 +0900

    quest 6

---
+++ Projects/Chapter_7/project_6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("%ld\n", sizeof(int));
+    printf("%ld\n", sizeof(short));
+    printf("%ld\n", sizeof(long));
+    printf("%ld\n", sizeof(float));
+    printf("%ld\n", sizeof(double));
+    printf("%ld\n", sizeof(long double));
+}
